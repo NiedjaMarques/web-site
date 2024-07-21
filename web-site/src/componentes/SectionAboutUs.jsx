4464a5cee184b4afeda8f960d1cd73d26b7191c8
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2024-07-21 16:17:38 -0300

    .

--- web-site/src/componentes/SectionAboutUs.jsx
+++ web-site/src/componentes/SectionAboutUs.jsx
@@ -1,28 +1,30 @@
 
+// import { FaAngleRight } from "react-icons/fa6";
 
 
-const SectionAboutUs = () => {
+const SectionAboutus = () => {
     return(
         <>
             <div>
-                <div className="w-1/2">
+                testando
+                {/* <div className="w-1/2">
                     <img src="" alt="" />
                 </div>
 
-                <div className="w-1/2">
-                    <button>
-
-                    </button>
-
+                <div className="w-1/2"> */}
+                    {/* <h6 className="text-xl px-4 py-2 my-4 text-center font-semibold text-roxo bg-roxo-transparente rounded-md">
+                        About Us
+                    </h6> */}
+{/* 
                     <h1>
                         How We Can Help You Achieve Your Business Goal
                     </h1>
 
                     <p>
                         Integer pretium molestie nisl, non blandit lectus suscipit in. Vivamus tellus diam, iaculis eget nulla sit amet, tincidunt consectetur sem. Suspendisse laoreet, quam sed faucibus feugiat, tortor velit suscipit orci, sed consectetur ante eros id urna. Mauris luctus nulla ut pharetra tempor.
-                    </p>
+                    </p> */}
 
-                    <p>
+                    {/* <p>
                         Mauris egestas eleifend sapien eu malesuada. Phasellus at metus eget sapien tristique accumsan non sit amet augue.
                     </p>
 
@@ -30,19 +32,19 @@ const SectionAboutUs = () => {
 
                     <h4>
                         CEO Jhone Doe
-                    </h4>
+                    </h4> */}
 
-                    <div className='flex items-center mb-8'>
+                    {/* <div className='flex items-center mb-8'>
 
                         <button className='bg-roxo rounded-full p-2 md:p-3 mr-2 shadow-xl'>
-                            <FaAngleRight className='text-lg text-white'/>
+                            {/* <FaAngleRight className='text-lg text-white'/> 
                         </button>
 
                         <a href="#" className='text-roxo font-medium'>Learn More</a>
-                    </div>
-                </div>
+                    </div> */}
+                {/* </div> */}
             </div>
         </>
     )
 }
-export default SectionAboutUs
+export default SectionAboutus
