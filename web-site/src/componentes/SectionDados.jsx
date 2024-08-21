e9b2b54da0919ff860ae62f3be53d9e57625e72c
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2024-08-21 12:48:54 -0300

    Uso de uma nova biblioteca

--- web-site/src/componentes/SectionDados.jsx
+++ web-site/src/componentes/SectionDados.jsx
@@ -1,9 +1,4 @@
-import project from '../../public/app/view/flaticon/risonho.png'
-import project1 from '../../public/app/view/flaticon/computador.png'
-import project2 from '../../public/app/view/flaticon/foguete.png'
-import project3 from '../../public/app/view/flaticon/fogos-de-artificio.png'
-import bg3 from '../../public/app/view/background/bg/bg3.png'
-import ScrollTrigger from 'react-scroll-trigger' //apagar do projeto
+// import ScrollTrigger from 'react-scroll-trigger' //apagar do projeto
 
 // const SectionDados = () => {
     
@@ -76,42 +71,52 @@ import ScrollTrigger from 'react-scroll-trigger' //apagar do projeto
 // }
 // export default SectionDados
 
-import { useEffect, useRef, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 import CountUp from 'react-countup'
+import bg3 from '../../public/app/view/background/bg/bg3.png'
+import project from '../../public/app/view/flaticon/risonho.png'
+import project1 from '../../public/app/view/flaticon/computador.png'
+import project2 from '../../public/app/view/flaticon/foguete.png'
+import project3 from '../../public/app/view/flaticon/fogos-de-artificio.png'
+//import { useEffect, useRef, useState } from 'react'
+
 
 const SectionDados = () => {
 
-    const [counterOn, setCounterOn] = useState(false)
-    const triggerRefs = useRef([])
+    const { ref: ref1, inView: inView1 } = useInView({ threshold: 0.5 });
+    const { ref: ref2, inView: inView2 } = useInView({ threshold: 0.5 });
+    const { ref: ref3, inView: inView3 } = useInView({ threshold: 0.5 });
+    const { ref: ref4, inView: inView4 } = useInView({ threshold: 0.5 });
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(([entry]) => {
-            if (entry.isIntersecting) {
-                setCounterOn(true);
-            } else {
-                setCounterOn(false);
-            }
-        }, { threshold: 0.5 });
 
-        triggerRefs.current.forEach(ref => {
-            if (ref) {
-                observer.observe(ref)
-            }
-        })
+    // useEffect(() => {
+    //     const observer = new IntersectionObserver(([entry]) => {
+    //         if (entry.isIntersecting) {
+    //             setCounterOn(true);
+    //         } else {
+    //             setCounterOn(false);
+    //         }
+    //     }, { threshold: 0.5 });
 
-        return () => {
-            triggerRefs.current.forEach(ref => {
-                if (ref) {
-                    observer.unobserve(ref)
-                }
-            })
-        }
-    }, [])
+    //     triggerRefs.current.forEach(ref => {
+    //         if (ref) {
+    //             observer.observe(ref)
+    //         }
+    //     })
+
+    //     return () => {
+    //         triggerRefs.current.forEach(ref => {
+    //             if (ref) {
+    //                 observer.unobserve(ref)
+    //             }
+    //         })
+    //     }
+    // }, [])
 
     return (
         <div style={{ backgroundImage: `url(${bg3})` }} className="bg-roxo bg-cover bg-no-repeat bg-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 px-4 w-full py-12 my-12 justify-around">
 
-            <div className='text-center my-4 bg-white p-10 hover:-translate-y-4 transition-all ease-linear duration-500 flex flex-col justify-center items-center rounded-2xl drop-shadow-2xl cursor-pointer'>
+            <div ref={ref1} className='text-center my-4 bg-white p-10 hover:-translate-y-4 transition-all ease-linear duration-500 flex flex-col justify-center items-center rounded-2xl drop-shadow-2xl cursor-pointer'>
 
                 <div className='mb-5 drop-shadow-2xl'>
                     <img className="w-16 h-16" src={project} alt="img smile" />
@@ -119,13 +124,13 @@ const SectionDados = () => {
                 <p className='pb-3 font-semibold text-xl'>
                     Satisfied Clients
                 </p>
-                <span ref={el => triggerRefs.current[0] = el} className='text-roxo text-3xl font-bold'>
-                    {counterOn && <CountUp start={0} end={9875} duration={2} separator="" delay={0} />}
+                <span className='text-roxo text-3xl font-bold'>
+                    {inView1 && <CountUp start={0} end={9875} duration={2} separator="" delay={0} />}
                 </span>
 
             </div>
 
-            <div className='text-center my-4 sm:relative sm:top-8 hover:-translate-y-4 transition-all ease-linear duration-500 bg-white p-10 flex flex-col justify-center items-center rounded-2xl drop-shadow-2xl cursor-pointer'>
+            <div ref={ref2} className='text-center my-4 sm:relative sm:top-8 hover:-translate-y-4 transition-all ease-linear duration-500 bg-white p-10 flex flex-col justify-center items-center rounded-2xl drop-shadow-2xl cursor-pointer'>
 
                 <div className='mb-5 drop-shadow-2xl' >
                     <img className="w-16 h-16" src={project1} alt="img smile" />
@@ -133,13 +138,13 @@ const SectionDados = () => {
                 <p className='pb-3 font-semibold text-xl'>
                     Project Completed
                 </p>
-                <span ref={el => triggerRefs.current[1] = el} className='text-roxo text-3xl font-bold'>
-                    {counterOn && <CountUp start={0} end={8765} duration={2} separator="" delay={0}/>}
+                <span className='text-roxo text-3xl font-bold'>
+                    {inView2 && <CountUp start={0} end={8765} duration={2} separator="" delay={0}/>}
                 </span>
                 
             </div>
 
-            <div className='text-center my-4 bg-white p-10 hover:-translate-y-4 transition-all ease-linear duration-500 flex flex-col justify-center items-center rounded-2xl drop-shadow-2xl cursor-pointer'>
+            <div ref={ref3} className='text-center my-4 bg-white p-10 hover:-translate-y-4 transition-all ease-linear duration-500 flex flex-col justify-center items-center rounded-2xl drop-shadow-2xl cursor-pointer'>
 
                 <div className='mb-5 drop-shadow-2xl' >
                     <img className="w-16 h-16" src={project2} alt="img smile" />
@@ -147,13 +152,13 @@ const SectionDados = () => {
                 <p className='pb-3 font-semibold text-xl'>
                     Project Lunched
                 </p>
-                <span ref={el => triggerRefs.current[2] = el} className='text-roxo text-3xl font-bold'>
-                    {counterOn && <CountUp start={0} end={7894} duration={2} separator="" delay={0}/>}
+                <span className='text-roxo text-3xl font-bold'>
+                    {inView3 && <CountUp start={0} end={7894} duration={2} separator="" delay={0}/>}
                 </span>
 
             </div>
 
-            <div className='text-center my-4 sm:relative sm:top-8 hover:-translate-y-4 transition-all ease-linear duration-500 bg-white p-10 flex flex-col justify-center items-center rounded-2xl drop-shadow-2xl cursor-pointer'>
+            <div ref={ref4} className='text-center my-4 sm:relative sm:top-8 hover:-translate-y-4 transition-all ease-linear duration-500 bg-white p-10 flex flex-col justify-center items-center rounded-2xl drop-shadow-2xl cursor-pointer'>
 
                 <div className='mb-5 drop-shadow-2xl' >
                     <img className="w-16 h-16" src={project3} alt="img smile" />
@@ -161,8 +166,8 @@ const SectionDados = () => {
                 <p className='pb-3 font-semibold text-xl'>
                     Years Completed
                 </p>
-                <span ref={el => triggerRefs.current[3] = el} className='text-roxo text-3xl font-bold'>
-                    {counterOn && <CountUp start={0} end={65} duration={2} separator="" delay={0}/>}
+                <span className='text-roxo text-3xl font-bold'>
+                    {inView4 && <CountUp start={0} end={65} duration={2} separator="" delay={0}/>}
                 </span>
 
             </div>
